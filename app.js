bad6e9ef37cbcc4a68e316e62c601d900d372cc9
Jericho Rivera <jerichorivera@gmail.com>
2021-08-09 23:54:14 +0000

    bumped version to 0.2.0

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const processList = require('./lib/processlist');
 const loadData = require('./lib/load');
 
 program
-    .version('0.1.0')
+    .version('0.2.0')
     .option('-D, --dir <directory>', 'Path to pt-stalk files')
     .option('-t, --task <task>', 'ServiceNow ticket number', 'percona');
 
