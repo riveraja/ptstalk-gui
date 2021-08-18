0af6197275de821f8b915f01f97f422dd9cc8793
Jericho Rivera <jerichorivera@gmail.com>
2021-08-18 11:54:04 +0000

    Merged API

--- db/mongo.js
+++ db/mongo.js
@@ -1,8 +1,19 @@
 const { MongoClient } =  require('mongodb');
+const util = require('util');
 
 require('dotenv').config();
 
-const uri = process.env.MONGOURI;
+// const uri = process.env.MONGOURI;
+var dbusercred = '';
+if (process.env.DBUSER !== '') {
+    dbusercred = util.format("%s@%s:", process.env.DBUSER, process.env.DBPASS);
+}
+
+const dbhost = process.env.DBHOST;
+const dbport = process.env.DBPORT;
+const authdb = process.env.AUTHDB;
+
+const uri = util.format("mongodb://%s%s:%d/%s", dbusercred, dbhost, dbport, authdb)
 
 const connection = new MongoClient(uri, {
     useNewUrlParser: true,
